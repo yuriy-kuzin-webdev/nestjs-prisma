67d26ff2132614fa5aff0db21e4678e7adab2378
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2024-08-11 20:44:53 +0200

    chore: nest generate service post

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UserService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
